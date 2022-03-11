93103632f4048954d7273b454628fe881be53389
Bober-G <80640293+Bober-G@users.noreply.github.com>
2022-03-11 09:27:20 +0100

    Add files via upload

---
+++ ood_task3/ood_task3/Databases/ExcellDatabase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    public class ExcellDatabase : Iprint
+    {
+        public string Names { get; }
+        public string DeathRates { get; }
+        public string InfectionRates { get; }
+        public string GenomeIds { get; }
+
+        public ExcellDatabase(string names, string deathRates, string infectionRates, string genomeIds)
+        {
+            Names = names;
+            DeathRates = deathRates;
+            InfectionRates = infectionRates;
+            GenomeIds = genomeIds;
+        }
+        
+        public void get_data(List<GenomeData> g)
+        {
+            string[] names = Names.Split(';');
+            string[] deathRates = DeathRates.Split(';');
+            string[] infectionRates = InfectionRates.Split(';');
+            string[] genomeIds = GenomeIds.Split(';');
+            //GenomeData genomeData = new GenomeData();
+            //for(int i = 0; i < deathRates.Length; i++)
+            //{
+            //    dr.Add(int.Parse(deathRates[i]));
+            //}
+            
+            Console.WriteLine("EXCEL DATABASE");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                for(int j = 0; j < g.Count; j++)
+                {
+                    string tmp = Convert.ToString(g[j].Id); 
+                    if(genomeIds[i].Equals(tmp))
+                        Console.WriteLine($"{names[i]}, {deathRates[i]}, {infectionRates[i]}, {g[j]}");
+                }
+
+               
+            }
+        }
+    }
+
+    public class SimpleDatabaseRow
+    {
+        public string VirusName { get; set; }
+        public double DeathRate { get; set; }
+        public double InfectionRate { get; set; }
+        public Guid GenomeId { get; set; }
+    }
+
+    public class SimpleDatabase: Iprint
+    {
+        public IReadOnlyList<SimpleDatabaseRow> Rows { get; }
+        public SimpleDatabase(IEnumerable<SimpleDatabaseRow> simpleDatabaseRows)
+        {
+            var list = new List<SimpleDatabaseRow>();
+            list.AddRange(simpleDatabaseRows);
+
+            Rows = list;
+        }
+
+        public void get_data(List<GenomeData> g)
+        {
+            List<SimpleDatabaseRow> list = new List<SimpleDatabaseRow>();
+            List<VirusData> virusDatas = new List<VirusData>();
+            
+            foreach(var x in Rows)
+            {
+                list.Add(x);
+            }
+            Console.WriteLine("\nSIMPLE DATABASE");
+            foreach (var x in list)
+            {
+                //for (int j = 0; j < g.Count; j++)
+                //{
+                //    string tmp = Convert.ToString(g[j].Id);
+                //    if (list.[i].Equals(tmp))
+                //        Console.WriteLine($"{names[i]}, {deathRates[i]}, {infectionRates[i]}, {g[j]}");
+                //}
+            }
+
+        }
+    }
+}
---
+++ ood_task3/ood_task3/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Task3.Subjects;
+using Task3.Vaccines;
+
+namespace Task3
+{
+    class Program
+    {
+        public class databaseData
+        {
+            public List<GenomeData> genomeDatas = new List<GenomeData>();
+            public List<string> names = new List<string>();
+            public List<double> deathRate = new List<double>();
+            public List<double> infectionRate = new List<double>();
+            public List<VirusData> virusDatas = new List<VirusData>();
+        }
+        public class MediaOutlet
+        {
+
+            public List<object> db = new List<object>();
+            public void addDb(object o)
+            {
+                db.Add(o);
+            }
+            public void Publish(List<GenomeData> g)
+            {
+                foreach (var d in db)
+                {
+                    if (d is Iprint)
+                    {
+                        Iprint p = d as Iprint;
+                        p.get_data(g);
+                    }
+                }
+            }
+        }
+
+        public class Tester
+        {
+            public void Test()
+            {
+                var vaccines = new List<IVaccine>() { new AvadaVaccine(), new Vaccinator3000(), new ReverseVaccine() };
+
+                foreach (var vaccine in vaccines)
+                {
+                    Console.WriteLine($"Testing {vaccine}");
+                    var subjects = new List<ISubject>();
+                    int n = 5;
+                    for (int i = 0; i < n; i++)
+                    {
+                        subjects.Add(new Cat($"{i}"));
+                        subjects.Add(new Dog($"{i}"));
+                        subjects.Add(new Pig($"{i}"));
+                    }
+
+                    foreach (var subject in subjects)
+                    {
+                        // process of vaccination
+                    }
+
+                    var genomeDatabase = Generators.PrepareGenomes();
+                    var simpleDatabase = Generators.PrepareSimpleDatabase(genomeDatabase);
+                    // iteration over SimpleGenomeDatabase using solution from 1)
+                    // subjects should be tested here using GetTested function
+
+
+                    // iterating over simpleDatabase
+                    //{
+                        //foreach (var subject in subjects)
+                        //{
+                        //    subject.GetTested();
+                        //}
+                    //}
+
+                    int aliveCount = 0;
+                    foreach (var subject in subjects)
+                    {
+                        if (subject.Alive) aliveCount++;
+                    }
+                    Console.WriteLine($"{aliveCount} alive!");
+                }
+            }
+        }
+        public static void Main(string[] args)
+        {
+            var genomeDatabase = Generators.PrepareGenomes();
+            var simpleDatabase = Generators.PrepareSimpleDatabase(genomeDatabase);
+            var excellDatabase = Generators.PrepareExcellDatabase(genomeDatabase);
+            var overcomplicatedDatabase = Generators.PrepareOvercomplicatedDatabase(genomeDatabase);
+            var mediaOutlet = new MediaOutlet();
+            mediaOutlet.addDb(excellDatabase);
+            mediaOutlet.addDb(simpleDatabase);
+            mediaOutlet.Publish(genomeDatabase.genomeDatas);
+            //excellDatabase.get_data(genomeDatabase.genomeDatas);
+
+
+
+
+            // testing animals
+            //var tester = new Tester();
+            //tester.Test();
+        }
+    }
+}
